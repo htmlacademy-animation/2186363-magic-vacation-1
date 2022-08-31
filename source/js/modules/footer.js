c1f4e7ec0d8701d991d9681bd958dc96ea5ddf16
Екатерина Хрипунова <cat123_93@mail.ru>
2022-08-31 12:45:16 +0300

    добавлена анимация для пагинации

--- source/js/modules/footer.js
+++ source/js/modules/footer.js
@@ -2,9 +2,12 @@ export default () => {
   let footerTogglers = document.querySelectorAll(`.js-footer-toggler`);
 
   if (footerTogglers.length) {
+
     for (let i = 0; i < footerTogglers.length; i++) {
       footerTogglers[i].addEventListener(`click`, function () {
+        console.log(footerTogglers[i], 'footer')
         let footer = footerTogglers[i].parentNode;
+
         if (footer.classList.contains(`screen__footer--full`)) {
           footer.classList.remove(`screen__footer--full`);
         } else {
